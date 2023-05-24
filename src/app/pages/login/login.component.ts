2c15a8efe460dec39aaa497304cef9399f01467d
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-05-25 00:22:09 +0530

    Added User list in localstorage for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -18,10 +18,31 @@ export class LoginComponent {
   }
 
   formSubmit(){
-    this.adminService.logIn(this.user).subscribe(res => {
-      console.log(res);
-    });
+    // Step 1: Retrieve the existing array from localStorage
+    const existingArrayString = localStorage.getItem('userList');
+    let existingArray: any[] = []; // Declare existingArray as a variable
+
+// Check if the existing array already exists in localStorage
+    if (existingArrayString) {
+      existingArray = JSON.parse(existingArrayString);
+    }
+
+    const emailExists = existingArray.some((obj: any) => obj.adminEmail === this.user.email);
+    const passwordExists = existingArray.some((obj: any) => obj.adminPassword === this.user.password);
+
+// Step 4: Output the result
+    console.log(`Email "${this.user.email}" exists in the array: ${emailExists}`);
+    if(emailExists && passwordExists){
+      alert("Login successful");
+    }else{
+      alert("Login failed");
+    }
+
+    // this.adminService.logIn(this.user).subscribe(res => {
+    //   console.log(res);
+    // });
 
   }
 
 }
+
